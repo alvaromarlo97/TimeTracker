bf34625c6298dea8783a806ae80b088e115d3336
alvaromartin <alvaro.martin.lorenzo@outlook.com>
2021-08-31 10:04:48 +0200

    Add index

--- backend/index.js
+++ backend/index.js
@@ -2,3 +2,18 @@ const express = require('express');
 require('dotenv').config();
 const debug = require('debug')('server');
 const morgan = require('morgan');
+const cors = require('cors');
+
+require('./src/config/ddbb.config');
+
+const server = express();
+const port = process.env.PORT || 5000;
+
+server.use(cors());
+server.use(morgan('dev'));
+server.use(express.json());
+
+server.listen(
+  port,
+  () => debug(`Server is running on ${`http://localhost:${port}`}`),
+);
