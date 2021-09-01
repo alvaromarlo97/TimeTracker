8ba5d9269bb861156eb1ad91e217212270368376
alvaromartin <alvaro.martin.lorenzo@outlook.com>
2021-09-01 17:08:49 +0200

    Add timer to main

--- frontend/TimeTracker/.eslintrc.js
+++ frontend/TimeTracker/.eslintrc.js
@@ -35,10 +35,9 @@ module.exports = {
     }],
     'react/jsx-filename-extension': [1, {
       extensions: [
-        '.js',
         '.jsx',
         '.tsx',
       ],
-    }, { allow: 'as-needed' }],
+    }],
   },
 };
--- frontend/TimeTracker/App.js
+++ frontend/TimeTracker/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-filename-extension */
 /* eslint-disable react/prop-types */
 /**
  * Sample React Native App
@@ -8,81 +9,14 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-import Login from './src/components/Login/Login';
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+// import Login from './src/components/Login/Login';
+import MyTimer from './src/components/Main/Main';
 
-const Section = ({ children, title }) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}
-      >
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}
-      >
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <Login></Login>
-  );
-};
+const App = () => (
+  <>
+    <MyTimer />
+  </>
+);
 
 export default App;
