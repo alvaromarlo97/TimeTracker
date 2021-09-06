0788721dd7caffe02ced81f7a68ef4c0cc13aa03
alvaromartin <alvaro.martin.lorenzo@outlook.com>
2021-09-06 15:13:34 +0200

    Add Background timer

--- frontend/TimeTracker/App.js
+++ frontend/TimeTracker/App.js
@@ -10,13 +10,14 @@
 
 import React from 'react';
 import { Provider } from 'react-redux';
-import Navegator from './src/components/Navegator/Navegator';
+// import Navegator from './src/components/Navegator/Navegator';
 import configureStore from './src/redux/store';
+import Background from './src/components/Moment/Moment';
 
 export default function App() {
   return (
     <Provider store={configureStore()}>
-      <Navegator />
+      <Background />
     </Provider>
   );
 }
--- frontend/TimeTracker/src/components/Moment/Moment.jsx
+++ frontend/TimeTracker/src/components/Moment/Moment.jsx
@@ -1,16 +1,85 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-
+  StyleSheet,
   Text,
-
+  Button,
+  SafeAreaView,
 } from 'react-native';
-import moment from 'moment';
+import BackgroundTimer from 'react-native-background-timer';
 
-export default function Moment() {
-  const [time, setTime] = useState(moment().startOf('h').fromNow());
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    backgroundColor: '#000',
+  },
+  time: {
+    fontSize: 30,
+    color: '#fff',
+    marginBottom: 30,
+    textAlign: 'center',
+  },
+});
+
+export default function Background() {
+  const [secondsLeft, setSecondsLeft] = useState(0);
+  const [timerOn, setTimerOn] = useState(false);
+  // Runs when timerOn value changes to start or stop timer
+  const clockify = () => {
+    const hours = Math.floor(secondsLeft / 60 / 60);
+    const mins = Math.floor((secondsLeft / 60) % 60);
+    const seconds = Math.floor(secondsLeft % 60);
+    const displayHours = hours < 10 ? `0${hours}` : hours;
+    const displayMins = mins < 10 ? `0${mins}` : mins;
+    const displaySecs = seconds < 10 ? `0${seconds}` : seconds;
+    return {
+      displayHours,
+      displayMins,
+      displaySecs,
+    };
+  };
+  useEffect(() => {
+    if (secondsLeft === 0) BackgroundTimer.stopBackgroundTimer();
+  }, [secondsLeft]);
+  const startTimer = () => {
+    BackgroundTimer.runBackgroundTimer(() => {
+      setSecondsLeft((secs) => {
+        if (secs >= 0) return secs + 1;
+        return 0;
+      });
+    }, 1000);
+  };
+  useEffect(() => {
+    if (timerOn) startTimer();
+    else BackgroundTimer.stopBackgroundTimer();
+    return () => {
+      BackgroundTimer.stopBackgroundTimer();
+    };
+  }, [timerOn]);
+  function log() {
+    console.log(clockify().displayHours, clockify().displayMins, clockify().displaySecs);
+  }
   return (
-    <Text>
-      {time}
-    </Text>
+    <SafeAreaView style={styles.container}>
+
+      <Text style={styles.time}>
+        {' '}
+        {clockify().displayHours}
+        {' '}
+        :
+        {' '}
+        {clockify().displayMins}
+        {' '}
+        :
+        {' '}
+        {clockify().displaySecs}
+        {' '}
+
+      </Text>
+      <Button title="Start/Stop" onPress={() => setTimerOn((timerOn) => !timerOn)} />
+      <Button title="console.log" onPress={() => log()} />
+
+    </SafeAreaView>
   );
 }
