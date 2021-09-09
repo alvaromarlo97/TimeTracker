c4f0f3599bd4db7e7e81718da99b004fdf3243b1
alvaromartin <alvaro.martin.lorenzo@outlook.com>
2021-09-09 12:35:51 +0200

    Add delete user activity

--- frontend/TimeTracker/src/components/Stats/Stats.jsx
+++ frontend/TimeTracker/src/components/Stats/Stats.jsx
@@ -1,92 +1,41 @@
-import {
-  LineChart,
-  BarChart,
-  PieChart,
-  ProgressChart,
-  ContributionGraph,
-  StackedBarChart,
-} from 'react-native-chart-kit';
 import React from 'react';
-import { Dimensions, View, Text } from 'react-native';
+import { useSelector } from 'react-redux';
+import { StyleSheet, View } from 'react-native';
+import { VictoryBar, VictoryChart, VictoryTheme } from 'victory-native';
 
-const chartConfig = {
-  backgroundColor: '#e26a00',
-  backgroundGradientFrom: '#fb8c00',
-  backgroundGradientTo: '#ffa726',
-  decimalPlaces: 2, // optional, defaults to 2dp
-  color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-  labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-  style: {
-    borderRadius: 16,
+const data = [
+  { quarter: 'fff', earnings: 13000 },
+  { quarter: 2, earnings: 16500 },
+  { quarter: 3, earnings: 14250 },
+  { quarter: 4, earnings: 19000 },
+];
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5fcff',
   },
-  propsForDots: {
-    r: '6',
-    strokeWidth: '2',
-    stroke: '#ffa726',
-  },
-};
-const data = {
-  labels: ['Test1', 'Test2'],
-  legend: ['L1', 'L2', 'L3'],
-  data: [
-    [60, 60, 60],
-    [30, 30, 60],
-  ],
-  barColors: ['#dfe4ea', '#ced6e0', '#a4b0be'],
-};
+});
+
 export default function Stats() {
+  const activities = useSelector(({ loggedUser }) => loggedUser?.user?.activities);
+  const name = activities?.map((element) => element.activityName);
+  const colors = activities?.map((element) => element.color);
+  // console.log(name);
+
+  // console.log('p');
+  const times = activities?.map((element) => element.activityTime);
+  const seconds = times?.map((e) => e?.map((i) => i.seconds));
+  //   console.log(seconds);
+  //   console.log(colors);
+
   return (
-    <View>
-      <Text>Bezier Line Chart</Text>
-      <LineChart
-        data={{
-          labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-          datasets: [
-            {
-              data: [
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-              ],
-            },
-          ],
-        }}
-        width={Dimensions.get('window').width} // from react-native
-        height={220}
-        yAxisLabel="$"
-        yAxisSuffix="k"
-        yAxisInterval={1} // optional, defaults to 1
-        chartConfig={{
-          backgroundColor: '#e26a00',
-          backgroundGradientFrom: '#fb8c00',
-          backgroundGradientTo: '#ffa726',
-          decimalPlaces: 2, // optional, defaults to 2dp
-          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-          style: {
-            borderRadius: 16,
-          },
-          propsForDots: {
-            r: '6',
-            strokeWidth: '2',
-            stroke: '#ffa726',
-          },
-        }}
-        bezier
-        style={{
-          marginVertical: 8,
-          borderRadius: 16,
-        }}
-      />
-      <StackedBarChart
-        data={data}
-        width={Dimensions.get('window').width}
-        height={220}
-        chartConfig={chartConfig}
-      />
+    <View style={styles.container}>
+      <VictoryChart width={350} theme={VictoryTheme.material}>
+        <VictoryBar data={data} x="quarter" y="earnings" />
+      </VictoryChart>
     </View>
   );
 }
